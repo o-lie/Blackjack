bee87fbf4730406ceebedc9ea99ba4bbda0fa494
Oskar <oskiflog@gmail.com>
2021-09-20 01:37:41 +0200

    scipt.js: Convert spaces into tabs

--- script.js
+++ script.js
@@ -18,109 +18,109 @@ let resetGameButton = document.querySelector(".buttons-container__button--reset-
 let rulesButton = document.querySelector(".buttons-container__button--open-rules");
 
 function randomCard() {
-    let card = Math.floor(Math.random() * 13) + 1;
-    if (card > 10) return 10
-    else if (card === 1) return 11
-    else return card
+	let card = Math.floor(Math.random() * 13) + 1;
+	if (card > 10) return 10
+	else if (card === 1) return 11
+	else return card
 }
 
 
 let startGame = () => {
-    gameBox.classList.toggle('game-container--show');
-    newCardButton.classList.toggle('buttons-container__button--show');
-    resetGameButton.classList.toggle('buttons-container__button--show');
-    nextRoundButton.classList.toggle('buttons-container__button--show');
-    startGameButton.classList.remove('buttons-container__button--show');
-    nextRound();
+	gameBox.classList.toggle('game-container--show');
+	newCardButton.classList.toggle('buttons-container__button--show');
+	resetGameButton.classList.toggle('buttons-container__button--show');
+	nextRoundButton.classList.toggle('buttons-container__button--show');
+	startGameButton.classList.remove('buttons-container__button--show');
+	nextRound();
 }
 
 let nextRound = () => {
-    if (!isAlive && cash != 0) {
-        isAlive = true;
-        hasBlackjack = false;
-        let firstCard = randomCard();
-        let secondCard = randomCard();
-        cards = [firstCard, secondCard];
-        sum = firstCard + secondCard;
-        cashBox.classList.remove('game-container__cash--red');
-        cashBox.classList.remove('game-container__cash--green');
-        renderGame();
-    }
+	if (!isAlive && cash != 0) {
+		isAlive = true;
+		hasBlackjack = false;
+		let firstCard = randomCard();
+		let secondCard = randomCard();
+		cards = [firstCard, secondCard];
+		sum = firstCard + secondCard;
+		cashBox.classList.remove('game-container__cash--red');
+		cashBox.classList.remove('game-container__cash--green');
+		renderGame();
+	}
 }
 let renderGame = () => {
 
-    // CONTENT
-    cardsBox.textContent = "Cards: ";
-    cards.forEach(function (card) {
-        cardsBox.textContent += `${card} `;
-    });
-    sumBox.textContent = `Sum: ${sum}`;
-    cashBox.textContent = `Cash: $${cash}`;
+	// CONTENT
+	cardsBox.textContent = "Cards: ";
+	cards.forEach(function (card) {
+		cardsBox.textContent += `${card} `;
+	});
+	sumBox.textContent = `Sum: ${sum}`;
+	cashBox.textContent = `Cash: $${cash}`;
 
-    // MESSAGES
-    if (sum === 0) messageBox.textContent = "Let's start the game!"
-    else if (sum === 20) messageBox.textContent = `You are missing 1 point. Good luck! Draw a new card.`
-    else if(sum < 21 && sum != 20) messageBox.textContent = `You are missing ${21 - sum} points. Draw a new card.`
-    else if (sum === 21) {
-        cash += 10;
-        hasBlackjack = true;
-        isAlive = false;
-        messageBox.textContent = "You have 21 points! You've got a blackjack!";
-        cashBox.textContent = `Cash: $${cash}`;
-        cashBox.classList.toggle('game-container__cash--green');
-    }
+	// MESSAGES
+	if (sum === 0) messageBox.textContent = "Let's start the game!"
+	else if (sum === 20) messageBox.textContent = `You are missing 1 point. Good luck! Draw a new card.`
+	else if(sum < 21 && sum != 20) messageBox.textContent = `You are missing ${21 - sum} points. Draw a new card.`
+	else if (sum === 21) {
+		cash += 10;
+		hasBlackjack = true;
+		isAlive = false;
+		messageBox.textContent = "You have 21 points! You've got a blackjack!";
+		cashBox.textContent = `Cash: $${cash}`;
+		cashBox.classList.toggle('game-container__cash--green');
+	}
 
-    else if (sum > 21) {
-        cash -= 5;
-        isAlive = false;
-        messageBox.textContent = `You've lost! You had too many points. Try again in the next round.`;
-        cashBox.textContent = `Cash: $${cash}`;
-        cashBox.classList.toggle('game-container__cash--red');
-    }
+	else if (sum > 21) {
+		cash -= 5;
+		isAlive = false;
+		messageBox.textContent = `You've lost! You had too many points. Try again in the next round.`;
+		cashBox.textContent = `Cash: $${cash}`;
+		cashBox.classList.toggle('game-container__cash--red');
+	}
 
-    if (cash === 0) {
-        messageBox.textContent = "You're out of the game!";
-        gameBox.classList.remove('.game-container--show');
-        newCardButton.classList.remove('buttons-container__button--show');
-        nextRoundButton.classList.remove('buttons-container__button--show');
-    }
+	if (cash === 0) {
+		messageBox.textContent = "You're out of the game!";
+		gameBox.classList.remove('.game-container--show');
+		newCardButton.classList.remove('buttons-container__button--show');
+		nextRoundButton.classList.remove('buttons-container__button--show');
+	}
 
 }
 
 let newCard = () => {
-    if (isAlive === true && hasBlackjack === false) {
-        let newcard = randomCard();
-        sum += newcard;
-        cards.push(newcard);
-        renderGame();
-    }
+	if (isAlive === true && hasBlackjack === false) {
+		let newcard = randomCard();
+		sum += newcard;
+		cards.push(newcard);
+		renderGame();
+	}
 }
 
 let resetGame = () => {
-    sum = 0;
-    cash = 20;
-    cards = [];
-    isAlive = false;
-    hasBlackjack = false;
+	sum = 0;
+	cash = 20;
+	cards = [];
+	isAlive = false;
+	hasBlackjack = false;
 
-    messageBox.textContent = "Let's start the game!";
+	messageBox.textContent = "Let's start the game!";
 
-    gameBox.classList.remove('game-container--show');
-    newCardButton.classList.remove('buttons-container__button--show');
-    resetGameButton.classList.remove('buttons-container__button--show');
-    nextRoundButton.classList.remove('buttons-container__button--show');
-    startGameButton.classList.toggle('buttons-container__button--show');
-    renderGame();
+	gameBox.classList.remove('game-container--show');
+	newCardButton.classList.remove('buttons-container__button--show');
+	resetGameButton.classList.remove('buttons-container__button--show');
+	nextRoundButton.classList.remove('buttons-container__button--show');
+	startGameButton.classList.toggle('buttons-container__button--show');
+	renderGame();
 }
 
 let openRules = () => {
-    let rulesBox = document.querySelector(".rules-container");
+	let rulesBox = document.querySelector(".rules-container");
 
-    rulesBox.classList.toggle('rules-container--show');
+	rulesBox.classList.toggle('rules-container--show');
 
-    if (openRulesBoolean) rulesButton.textContent = 'Close rules'
-    else rulesButton.textContent = 'Open rules'
-    openRulesBoolean = !openRulesBoolean;
+	if (openRulesBoolean) rulesButton.textContent = 'Close rules'
+	else rulesButton.textContent = 'Open rules'
+	openRulesBoolean = !openRulesBoolean;
 }
 
 
